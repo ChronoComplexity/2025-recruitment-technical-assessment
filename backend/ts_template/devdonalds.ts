febe75761dd750e4bd3917a90e8384effc9c15fc
Jenny <liu1jenny@gmail.com>
2025-02-13 18:49:34 +1100

    Completed first devdonalds typescript function

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -45,14 +45,39 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  const dashes = /[-_]/g;
+  const invalidAlphabet = /[^a-zA-Z\s]/g;
+
+  // If no recipe
+  if (!recipeName) {
+    return null;
+  }
+
+  // Removes all the invalid characters (dashes must be first so they're counted as s+)
+  recipeName = recipeName.replace(dashes, ' ');
+  recipeName = recipeName.replace(invalidAlphabet, '');
+  recipeName = recipeName.replace(/\s+/g, ' ');
+
+  // Sets the cases correctly for each word then rejoins them
+  let recipeWords = recipeName.split(" ");
+  recipeWords = recipeWords.map(word => word.slice(0,1).toLocaleUpperCase() + word.slice(1).toLocaleLowerCase());
+  recipeName = recipeWords.join(" ");
+
+  return recipeName;
 }
+
+/**
+ * - **type** can only be "recipe" or "ingredient".
+- **cookTime** can only be greater than or equal to 0
+- entry **names** must be unique
+- Recipe **requiredItems** can only have one element per name.
+ */
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
   // TODO: implement me
+  
   res.status(500).send("not yet implemented!")
 
 });
