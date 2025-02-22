7e050932669e24d32894f4074be8f11693e0a2aa
Jenny <liu1jenny@gmail.com>
2025-02-23 10:57:41 +1100

    Updated typescript function to match the spec's return values, and account for missed cases

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+const NON_EXISTING = -1;
 
 // ==== Type Definitions, feel free to add or modify ==========================
 interface cookbookEntry {
@@ -11,18 +12,24 @@ interface requiredItem {
   quantity: number;
 }
 
-export interface recipe extends cookbookEntry {
+interface recipe extends cookbookEntry {
   requiredItems: requiredItem[];
 }
 
-export interface ingredient extends cookbookEntry {
+interface ingredient extends cookbookEntry {
   cookTime: number;
 }
 
 // Datastore interface definition
-export interface cookbook {
-  recipes: recipe[];
-  ingredients: ingredient[];
+interface cookbook {
+  saved_recipes: recipe[];
+  saved_ingredients: ingredient[];
+}
+
+interface recipeSummary {
+  name: string;
+  ingredients: requiredItem[];
+  cookTime: number;
 }
 
 // =============================================================================
@@ -32,7 +39,7 @@ const app = express();
 app.use(express.json());
 
 // Cookbook object to store recipe
-const cookbook = { recipes: [] as recipe[], ingredients: [] as ingredient[] } as cookbook;
+const cookbook = { saved_recipes: [] as recipe[], saved_ingredients: [] as ingredient[] } as cookbook;
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -59,16 +66,21 @@ const parse_handwriting = (recipeName: string): string | null => {
     return null;
   }
 
-  // Removes all the invalid characters (dashes must be first so they're counted as s+)
+  // Removes all the invalid characters (dashes must be first so they're counted as spaces later)
   recipeName = recipeName.replace(dashes, ' ');
   recipeName = recipeName.replace(invalidAlphabet, '');
   recipeName = recipeName.replace(/\s+/g, ' ');
+  recipeName = recipeName.trim();
 
   // Splits the string, sets the cases correctly for each word then rejoins it
   let recipeWords = recipeName.split(" ");
   recipeWords = recipeWords.map(word => word.slice(0,1).toLocaleUpperCase() + word.slice(1).toLocaleLowerCase());
   recipeName = recipeWords.join(" ");
 
+  // If word was all invalid characters
+  if (recipeName.length == 0) {
+    return null;
+  }
   return recipeName;
 }
 
@@ -89,15 +101,15 @@ app.post("/entry", (req:Request, res:Response) => {
 /**
  * Adds an entry to the cookbook if the entry name is unique, the type is either
  * recipe or ingredient, and if it's a recipe, there are no duplicate elements in its
- * required items or if it's an ingredient it's cook time is above 0.
+ * required items or, if it's an ingredient, its cook time is above 0.
  * 
  * @param input Either a recipe or an ingredient and their details as input
  * @returns empty object
  */
 const addEntry = (input: recipe & ingredient): {} => {
   // Filters out non-unique entry names
-  if (cookbook.recipes.some(entry => (entry.name === input.name)) ||
-  cookbook.ingredients.some(entry => (entry.name === input.name))) {
+  if (cookbook.saved_recipes.some(entry => (entry.name === input.name)) ||
+  cookbook.saved_ingredients.some(entry => (entry.name === input.name))) {
     throw new Error('Entry names must be unique, entry name already taken.');
   }
 
@@ -105,16 +117,16 @@ const addEntry = (input: recipe & ingredient): {} => {
   if (input.type === "recipe") {
     // Checks for duplicate names in required items
     const requiredNames = input.requiredItems.map(entry => entry.name);
-    if (requiredNames.some((entry, index) => index !== requiredNames.indexOf(entry))) {
+    if (new Set(requiredNames).size != requiredNames.length) {
       throw new Error('Recipe requiredItems can only have one element per name.');
     }
-    cookbook.recipes.push(input);
+    cookbook.saved_recipes.push({ name: input.name, type: input.type, requiredItems: input.requiredItems });
   } else if (input.type === "ingredient") {
     // Checks cookTime is above or equal to 0
     if (input.cookTime < 0) {
       throw new Error('cookTime can only be greater than or equal to 0');
     }
-    cookbook.ingredients.push(input);
+    cookbook.saved_ingredients.push({name: input.name, type: input.type, cookTime: input.cookTime});
   } else {
     throw new Error('Type can only be "recipe" or "ingredient".');
   }
@@ -143,47 +155,57 @@ app.get("/summary", (req:Request, res:Request) => {
  * @param name The name of the entry that we are trying to retrieve from the cookbook.
  * @returns requestedRecipe, the corresponding recipe requested
  */
-const getEntry = (name: string): recipe => {
+const getEntry = (name: string): recipeSummary => {
   // Checks for recipe with a matching name in the cookbook
-  const requestedRecipe = cookbook.recipes.find(recipe => recipe.name === name);
-  if (!requestedRecipe) {
-    throw new Error('Recipe with corresponding name not found.');
-  }
+  const requestedRecipe = cookbook.saved_recipes.find(recipe => recipe.name === name);
 
   // Checks all required items in recipe are also valid
-  if (!validateRecipe(name)) {
-    throw new Error('The recipe contains ingredients or recipes that are not in the cookbook.');
-  }
-
-  return requestedRecipe;
+  const ingredients = [] as requiredItem[];
+  return getRecipeInfo(name, ingredients, 0);
 }
 
 /**
  * Function that checks whether a recipe and all its ingredients and recipes
- * used to create it exist in the cookbook.
+ * used to create it exist in the cookbook, and adds its overall ingredients
+ * list and cook time to the given ingredient list and cookTime. Returns updated
+ * ingredient list, name and cook time as a recipe summary.
+ * Throws an error if the recipe does not exist.
  * 
  * @param name The name of the recipe being checked for.
- * @returns boolean stating whether the recipe and all its items are valid
+ * @param ingredients The ingredients already present in the recipe.
+ * @param cookTime The amount of time required to cook the recipe so far.
+ * @returns recipeSummary stating the recipe's name, updated ingredients list and
+ * cook time, accounting for the previous ingredient list and cooktime.
  */
-const validateRecipe = (name: string): boolean => {
-  const requestedRecipe = cookbook.recipes.find(recipe => recipe.name === name);
-  // Filters out all required items that are ingredients in the cookbook.
-  const requiredRecipeItems = requestedRecipe.requiredItems.filter(item => !cookbook.ingredients.some(
-  ingredient => ingredient.name === item.name));
-
-  // If there is a non-recipe left in the list, then it must be an invalid item.
-  if (requiredRecipeItems.some(item => !cookbook.recipes.some(
-    recipe => recipe.name === item.name))) {
-    return false;
+const getRecipeInfo = (name: string, ingredients: requiredItem[], cookTime: number): recipeSummary => {
+  // Checks recipe exists
+  const requestedRecipe = cookbook.saved_recipes.find(recipe => recipe.name === name);
+  if (!requestedRecipe) {
+    throw new Error('Recipe with corresponding name not found.');
   }
 
-  // If any of the recipes within the main recipe contains invalid items,
-  //recipe is invalid.
-  if (requiredRecipeItems.some(recipe => !validateRecipe(recipe.name))) {
-    return false;
+  // Handles items in recipe
+  for (const item of requestedRecipe.requiredItems) {
+    // Accounts for ingredient of recipe
+    const ingredient = cookbook.saved_ingredients.find(ingredient => ingredient.name === item.name);
+    if (ingredient) {
+      const match = ingredients.findIndex(existing => existing.name == item.name);
+      // Adds ingredient to ingredients list, creating a new object if needed, then updates cook time.
+      if (match == NON_EXISTING) {
+         ingredients.push({name: ingredient.name, quantity: item.quantity});
+      } else {
+        ingredients[match].quantity += item.quantity;
+      }
+      cookTime += item.quantity * ingredient.cookTime;
+    } else {
+      // Accounts for recipe corresponding to item, or throws error if not valid recipe
+      const result = getRecipeInfo(item.name, ingredients, cookTime);
+      ingredients = result.ingredients;
+      cookTime = result.cookTime;
+    }
   }
   
-  return true;
+  return { name, ingredients, cookTime};
 }
 // =============================================================================
 // ==== DO NOT TOUCH ===========================================================
