5452e138ab1c4c1ca5ba7a190138aea5a7136e54
Jenny <liu1jenny@gmail.com>
2025-02-23 10:28:02 +1100

    Updated tests to properly match task specifications, and cover all cases

--- backend/autotester/src/test.spec.ts
+++ backend/autotester/src/test.spec.ts
@@ -8,18 +8,38 @@ describe("Task 1", () => {
         .send({ input: inputStr });
     };
 
-    it("example1", async () => {
+    it("Non alphabetical characters removed", async () => {
       const response = await getTask1("Riz@z RISO00tto!");
       expect(response.body).toStrictEqual({ msg: "Rizz Risotto" });
     });
 
-    it("example2", async () => {
-      const response = await getTask1("alpHa-alFRedo");
-      expect(response.body).toStrictEqual({ msg: "Alpha Alfredo" });
+    it("Dashes and underscores replaced with spaces", async () => {
+      const response = await getTask1("alpHa-alFRedo_here");
+      expect(response.body).toStrictEqual({ msg: "Alpha Alfredo Here" });
     });
 
-    it("error case", async () => {
+    it("Multiple spaces removed and replaced with single space", async () => {
+      const response = await getTask1("alpHa_-alFRedo    -    here");
+      expect(response.body).toStrictEqual({ msg: "Alpha Alfredo Here" });
+    });
+
+    it("Whitespace from the edges removed", async () => {
+      const response = await getTask1("           here    ");
+      expect(response.body).toStrictEqual({ msg: "Here" });
+    });
+
+    it("Whitespace from the edges due to dashes and underscores being present removed", async () => {
+      const response = await getTask1("_____here----");
+      expect(response.body).toStrictEqual({ msg: "Here" });
+    });
+
+    it("error case, no input", async () => {
       const response = await getTask1("");
+      expect(response.status).toBe(400);
+    });
+
+    it("error case, invalid characters input only", async () => {
+      const response = await getTask1("123@");
       expect(response.status).toBe(400);
     });
   });
@@ -53,7 +73,7 @@ describe("Task 2", () => {
       expect(resp1.status).toBe(200);
     });
 
-    it("Congratulations u burnt the pan pt2", async () => {
+    it("Negative cookTime ingredient", async () => {
       const resp = await putTask2({
         type: "ingredient",
         name: "beef",
@@ -62,7 +82,7 @@ describe("Task 2", () => {
       expect(resp.status).toBe(400);
     });
 
-    it("Congratulations u burnt the pan pt3", async () => {
+    it("Incorrect type", async () => {
       const resp = await putTask2({
         type: "pan",
         name: "pan",
@@ -71,7 +91,7 @@ describe("Task 2", () => {
       expect(resp.status).toBe(400);
     });
 
-    it("Unique names", async () => {
+    it("Names are not unique", async () => {
       const resp = await putTask2({
         type: "ingredient",
         name: "Beef",
@@ -89,7 +109,16 @@ describe("Task 2", () => {
       const resp3 = await putTask2({
         type: "recipe",
         name: "Beef",
-        cookTime: 8,
+        requiredItems: [{ name: "Beef", quantity: 1 }]
+      });
+      expect(resp3.status).toBe(400);
+    });
+  
+    it("Names are not unique within the required items of a recipe", async () => {
+      const resp3 = await putTask2({
+        type: "recipe",
+        name: "Double Beef",
+        requiredItems: [{ name: "Beef", quantity: 1 }, { name: "Beef", quantity: 1 }]
       });
       expect(resp3.status).toBe(400);
     });
@@ -156,6 +185,112 @@ describe("Task 3", () => {
 
       const resp3 = await getTask3("Skibidi");
       expect(resp3.status).toBe(200);
+      expect(resp3.body).toStrictEqual({
+        "name": "Skibidi",
+        "cookTime": 2,
+        "ingredients": [
+          {
+          "name": "Bruh",
+          "quantity": 1
+          }
+        ]
+      });
+    });
+
+    it("Two of the same ingredients", async () => {
+      const meatball = {
+        type: "recipe",
+        name: "Apples",
+        requiredItems: [{ name: "Apple", quantity: 2 }],
+      };
+      const resp1 = await postEntry(meatball);
+      expect(resp1.status).toBe(200);
+
+      const resp2 = await postEntry({
+        type: "ingredient",
+        name: "Apple",
+        cookTime: 2,
+      });
+      expect(resp2.status).toBe(200);
+
+      const resp3 = await getTask3("Apples");
+      expect(resp3.status).toBe(200);
+      expect(resp3.body).toStrictEqual({
+        "name": "Apples",
+        "cookTime": 4,
+        "ingredients": [
+          {
+          "name": "Apple",
+          "quantity": 2
+          }
+        ]
+      });
+    });
+    it("Two different ingredients", async () => {
+      const meatball = {
+        type: "recipe",
+        name: "Banana splits",
+        requiredItems: [{ name: "Banana", quantity: 1 }, { name: "Split", quantity: 1 }],
+      };
+      const resp1 = await postEntry(meatball);
+      expect(resp1.status).toBe(200);
+
+      const resp2 = await postEntry({
+        type: "ingredient",
+        name: "Banana",
+        cookTime: 2,
+      });
+
+      expect(resp2.status).toBe(200);
+      const resp3 = await postEntry({
+        type: "ingredient",
+        name: "Split",
+        cookTime: 1,
+      });
+      expect(resp3.status).toBe(200);
+
+      const resp4 = await getTask3("Banana splits");
+      expect(resp4.status).toBe(200);
+      expect(resp4.body).toStrictEqual({
+        "name": "Banana splits",
+        "cookTime": 3,
+        "ingredients": [
+          {
+          "name": "Banana",
+          "quantity": 1
+          },
+          {
+            "name": "Split",
+            "quantity": 1
+          }
+        ]
+      });
+    });
+    it("Nested recipes", async () => {
+      const meatball = {
+        type: "recipe",
+        name: "Ice cream special",
+        requiredItems: [{ name: "Banana splits", quantity: 1 }, { name: "Split", quantity: 1 }],
+      };
+      const resp1 = await postEntry(meatball);
+      expect(resp1.status).toBe(200);
+
+      const resp4 = await getTask3("Ice cream special");
+      expect(resp4.status).toBe(200);
+      expect(resp4.body).toStrictEqual({
+        "name": "Ice cream special",
+        "cookTime": 4,
+        "ingredients": [
+          {
+          "name": "Banana",
+          "quantity": 1
+          },
+          {
+            "name": "Split",
+            "quantity": 2
+          }
+        ]
+      });
     });
   });
 });
